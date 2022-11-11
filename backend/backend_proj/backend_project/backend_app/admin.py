0e23439014ca4f6ea27de4e5caa8decc6321f827
mshnschnko <misha321654987i@gmail.com>
2022-11-11 15:14:49 +0300

    fixed password hash-codes and GET, POST requests

--- backend/backend_proj/backend_project/backend_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Users)
-admin.site.register(Messages)
-admin.site.register(Tasks)
-admin.site.register(Student_Teacher)
